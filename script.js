650fb5d3356cf5913a5501416406a03e3a1bbc25
StanislavDotDll <63843646+StanislavDotDll@users.noreply.github.com>
2020-11-03 23:08:10 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,93 +1,61 @@
-
 //google maps/////////////////////////////////////////////////////////////
-const labels = "";
-let labelIndex = 0;
-
 function initMap() {
-  const bangalore = {
-    lat: 50.6,
-    lng: 26.25,
-  };
-  const rivne = {
-    lat: 48.6,
-    lng: 26.25,
-  };
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    center: bangalore,
+    zoom: Zoom,
+    center: Center,
   });
-
-  addMarker(rivne, map);
-}
-
-
-function myFunction(arr) {
-  var out = "";
-  var i;
-  for (i = 0; i < arr.length; i++) {
-    console.log(arr)
-    out += '<a href="' + arr[i].url + '">' +
-      arr[i].display + '</a><br>';
-  }
-  document.getElementById("id01").innerHTML = out;
-}
-
-
-function addMarker(location, map) {
-
-  new google.maps.Marker({
-    position: location,
-    label: labels[labelIndex++ % labels.length],
-    map: map,
+  const labels = "";
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
   });
-
+  new MarkerClusterer(map, markers, {
+    imagePath: "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
 }
-
-
-
+var Zoom = 6;
+var Center = {
+  lat: 49.2,
+  lng: 30.5
+};
+var locations = [{}];
 
 //privat24//////////////////////////////////////////////////////
 
 var xhr = new XMLHttpRequest();
-
-xhr.open('GET', 'infrastructure.json', false);
-
+xhr.open('GET', 'infrastructure.json', false); //Можна поміняти на ссилку в любий момент просто під час створення цього коду я не знав як боротися з помилкою CORSE
 xhr.send();
-
-
-
 var arr = JSON.parse(xhr.response).devices;
-console.log(arr[7247].cityUA);
-
-
-
-
-
 
 //Search//////////////////////////////////////////////////////
 function search() {
-  let FindText=document.getElementById('searchText').value;
+  let FindText = document.getElementById('searchText').value;
   if (FindText == "") {
     alert("Enter city name (UA/RU/EN)");
   } else {
-    document.getElementById("ATMs").innerHTML="";
-    arr.forEach(function (item, i, arr)
-    {
-       if(item.cityUA==FindText||item.cityRU==FindText||item.cityEN==FindText)
-       {
-         console.log(item);
+    document.getElementById("ATMs").innerHTML = "";
+    locations = [];
+    arr.forEach(function (item, i, arr) {
+      if (item.cityUA == FindText || item.cityRU == FindText || item.cityEN == FindText) {
+        var loc = {
+          lng: parseFloat(item.latitude),
+          lat: parseFloat(item.longitude)
+        };
+        var atm = document.createElement("div");
+        atm.className = "Atm";
+        atm.id = JSON.stringify(loc);
+        atm.innerHTML = "<a onClick=''></a>" + item.fullAddressUa + "</a>";
+        document.getElementById("CityName").innerHTML = item.cityUA + ":";
+        document.getElementById("ATMs").appendChild(atm);
 
-         var atm=document.createElement("div");
-         atm.className="Atm";
-         atm.innerHTML=item.fullAddressUa;
-         document.getElementById("CityName").innerHTML=item.cityUA+":";
-         document.getElementById("ATMs").appendChild(atm);
-       }
+        locations[i + 1] = loc;
+        Center = loc;
+        Zoom = 12;
+        initMap();
+      }
     });
+
   }
-
 };
-
-
-
-
